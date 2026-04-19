b928dc0a161871318310e869e90f5a600e3d8554
Nathan Rous <ndrous2084@gmail.com>
2026-04-19 02:40:33 -0400

    scroll snap for hero page implemented

---
+++ frontend/crohns-colitis-website/src/app/pages/hero/components/news-and-events/news-and-events.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-and-events',
+  imports: [],
+  templateUrl: './news-and-events.html',
+  styleUrl: './news-and-events.scss',
+})
+export class NewsAndEvents {}
--- frontend/crohns-colitis-website/src/app/pages/hero/hero.ts
+++ frontend/crohns-colitis-website/src/app/pages/hero/hero.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { QuickNavCard } from "./components/quick-nav-card/quick-nav-card";
 import { DonationCard } from "./components/donation-card/donation-card";
+import { NewsAndEvents } from "./components/news-and-events/news-and-events";
 
 @Component({
   selector: 'app-hero',
-  imports: [QuickNavCard, DonationCard],
+  imports: [QuickNavCard, DonationCard, NewsAndEvents],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
