c481fabe4b2b797581a4b20a599707272e8bbc24
Nathan Rous <ndrous2084@gmail.com>
2026-04-21 17:53:28 -0400

    rough draft of hero page finished

--- frontend/crohns-colitis-website/src/app/components/header/header.ts
+++ frontend/crohns-colitis-website/src/app/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchBar } from "../search-bar/search-bar";
+import { NavBar } from "../nav-bar/nav-bar";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SearchBar, NavBar],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
