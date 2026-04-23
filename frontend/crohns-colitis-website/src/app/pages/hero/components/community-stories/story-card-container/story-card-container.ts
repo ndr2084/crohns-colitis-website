e3a0328b9c2804a67e0e6c0491ae24a155e6f441
Nathan Rous <ndrous2084@gmail.com>
2026-04-23 01:25:35 -0400

    refining layouts for mobile tablet and desktop

--- frontend/crohns-colitis-website/src/app/pages/hero/components/community-stories/story-card-container/story-card-container.ts
+++ frontend/crohns-colitis-website/src/app/pages/hero/components/community-stories/story-card-container/story-card-container.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-story-card-container',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './story-card-container.html',
   styleUrl: './story-card-container.scss',
 })
