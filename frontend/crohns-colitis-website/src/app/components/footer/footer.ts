0c63cc352a1993779f8782f1a8d6b63f6177ebf9
Nathan Rous <ndrous2084@gmail.com>
2026-05-07 20:33:49 -0400

    latest stable

--- frontend/crohns-colitis-website/src/app/components/footer/footer.ts
+++ frontend/crohns-colitis-website/src/app/components/footer/footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SocialMediaBar } from "../social-media-bar/social-media-bar";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [SocialMediaBar],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
--- frontend/crohns-colitis-website/src/app/pages/hero/hero.ts
+++ frontend/crohns-colitis-website/src/app/pages/hero/hero.ts
@@ -7,10 +7,11 @@ import { RelatedPages } from "./components/related-pages/related-pages";
 import { QuickNavSection } from "./components/quick-nav-section/quick-nav-section";
 import { NavBarMobile } from "../../components/nav-bar-mobile/nav-bar-mobile";
 import { MiscSection } from "./components/misc-section/misc-section";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-hero',
-  imports: [NewsAndEvents, HeroSection, CommunityStories, CommunityInformation, RelatedPages, QuickNavSection, NavBarMobile, MiscSection],
+  imports: [NewsAndEvents, HeroSection, CommunityStories, CommunityInformation, RelatedPages, QuickNavSection, NavBarMobile, MiscSection, Footer],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
